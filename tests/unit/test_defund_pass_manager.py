f5d786af5a562fb8b1e00d6660c983c1d24a2366
aaron-cesaro <aaron.cesaro@gmail.com>
2022-01-26 18:17:26 +0100

    split pass manager unit and integration tests

--- tests/unit/test_defund_pass_manager.py
+++ tests/unit/test_defund_pass_manager.py
@@ -104,24 +104,3 @@ def test_add_league_member_when_not_already_member(IMAGE_PATH, VENTURE_LEAGUE_RO
         defund_pass_manager.addLeagueMember(
             badge_awardee, token_image, league, role, {"from": account}
         )
-
-
-def test_token_uri_is_not_empty(IMAGE_PATH):
-    # Arrange
-    if network.show_active() not in LOCAL_BLOCKCHAIN_ENVIRONMENTS:
-        pytest.skip()
-    account = get_account()
-    defund_pass = deploy_defund_pass()
-    defund_pass_manager = deploy_defund_pass_manager(defund_pass.address)
-    defund_pass.transferOwnership(defund_pass_manager.address, {"from": account})
-    badge_awardee = get_account(1)
-    # Act
-    token_image = upload_to_ipfs(IMAGE_PATH.format("venture"))
-    tx_add_standard_member = defund_pass_manager.addStandardMember(
-        badge_awardee, token_image, {"from": account}
-    )
-    tx_add_standard_member.wait(1)
-    token_uri = defund_pass.tokenURI(tx_add_standard_member.return_value)
-    print(f"TokenURI: {token_uri}")
-    # Assert
-    assert token_uri != ""
